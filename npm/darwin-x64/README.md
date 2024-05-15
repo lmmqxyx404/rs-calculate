b86f3e45afbcb7b0f08f2bf81b4ae5431859653c
lmmqxyx <1368470804@qq.com>
2024-05-15 17:44:29 +0800

    initialize the project

---
+++ npm/darwin-x64/README.md
@@ -0,0 +1,3 @@
+# `calculate-rs-darwin-x64`
+
+This is the **x86_64-apple-darwin** binary for `calculate-rs`
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![deny(clippy::all)]
+
+#[macro_use]
+extern crate napi_derive;
+
+#[napi]
+pub fn sum(a: i32, b: i32) -> i32 {
+  a + b
+}
+
+#[napi]
+pub fn reduce(a: i32, b: i32) -> i32 {
+  a - b
+}
+
+#[napi]
+pub fn multiply(a: i32, b: i32) -> i32 {
+  a * b
+}
